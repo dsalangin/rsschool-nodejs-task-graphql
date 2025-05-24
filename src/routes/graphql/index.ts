469b8edba715e9c9e3d9e4622c45c12ac9b9ec48
dsalangin <dsalangin74@gmail.com>
2025-05-24 17:47:49 +0300

    feat: validate depth

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,8 +1,9 @@
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
 import { createGqlResponseSchema, gqlResponseSchema } from './schemas.js';
-import { graphql, GraphQLSchema } from 'graphql';
+import { graphql, GraphQLSchema, parse, validate } from 'graphql';
 import { RootQueryType } from './queries/root-query.js';
 import { Mutations } from './mutations/mutations.js';
+import depthLimit from 'graphql-depth-limit';
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
   const { prisma } = fastify;
@@ -17,11 +18,21 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
       },
     },
     async handler(req) {
+      const errors = validate(
+        schema,
+        parse(req.body.query),
+        [depthLimit(5)]
+      );
+
+      if (errors.length > 0) {
+        return { errors };
+      }
+
       return graphql({
         schema,
         source: req.body.query,
         variableValues: req.body.variables,
-        contextValue: { prisma }
+        contextValue: { prisma },
       });
     },
   });
